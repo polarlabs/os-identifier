41c29a087f7d577f2cfbb058eb4b253e59febc15
tokcum <tobias.mucke@gmail.com>
2025-10-25 17:06:13 +0200

    add: docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,17 @@
+//! # OS Identifier
+//!
+//! OS Identifier resolves product / release names of operating systems used by
+//! endoflife.date into canonical names.
+//!
+//! ```
+//! use os_identifier::Windows;
+//!
+//! fn main() {
+//!     let os_strings = Windows::try_from("11-24h2-w").unwrap();
+//!
+//!     assert!(os_strings.to_string().contains(&String::from("Microsoft Windows 11 Pro 24H2")));
+//! }
+//! ```
+
 mod windows;
 pub use windows::Windows;
