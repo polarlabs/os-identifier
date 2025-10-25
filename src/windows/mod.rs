5c54bbe3f2e1b5752c319757a39a3471e8287091
tokcum <tobias.mucke@gmail.com>
2025-10-25 16:34:50 +0200

    fix: pub enum with private variants

--- src/windows/mod.rs
+++ src/windows/mod.rs
@@ -1,3 +1,10 @@
+mod windows_7;
+mod windows_8;
+mod windows_10;
+mod windows_11;
+mod windows_vista;
+mod windows_xp;
+
 use windows_7::Windows7;
 use windows_8::Windows8;
 use windows_10::Windows10;
@@ -5,20 +12,26 @@ use windows_11::Windows11;
 use windows_vista::WindowsVista;
 use windows_xp::WindowsXP;
 
-mod windows_7;
+#[derive(Debug)]
+pub struct Windows(WindowsClientOS);
 
-mod windows_8;
+impl Windows {
+    pub fn to_string(&self) -> Vec<String> {
+        self.0.to_string()
+    }
+}
 
-mod windows_10;
+impl TryFrom<&str> for Windows {
+    type Error = String;
 
-mod windows_11;
-
-mod windows_vista;
-
-mod windows_xp;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let os = WindowsClientOS::try_from(value)?;
+        Ok(Self(os))
+    }
+}
 
 #[derive(Debug)]
-pub enum Windows {
+enum WindowsClientOS {
     Windows7(Windows7),
     Windows8(Windows8),
     Windows10(Windows10),
@@ -27,35 +40,35 @@ pub enum Windows {
     WindowsXP(WindowsXP),
 }
 
-impl Windows {
+impl WindowsClientOS {
     pub fn to_string(&self) -> Vec<String> {
         match self {
-            Windows::Windows7(windows) => windows.to_string(),
-            Windows::Windows8(windows) => windows.to_string(),
-            Windows::Windows10(windows) => windows.to_string(),
-            Windows::Windows11(windows) => windows.to_string(),
-            Windows::WindowsVista(windows) => windows.to_string(),
-            Windows::WindowsXP(windows) => windows.to_string(),
+            WindowsClientOS::Windows7(windows) => windows.to_string(),
+            WindowsClientOS::Windows8(windows) => windows.to_string(),
+            WindowsClientOS::Windows10(windows) => windows.to_string(),
+            WindowsClientOS::Windows11(windows) => windows.to_string(),
+            WindowsClientOS::WindowsVista(windows) => windows.to_string(),
+            WindowsClientOS::WindowsXP(windows) => windows.to_string(),
         }
     }
 }
 
-impl TryFrom<&str> for Windows {
+impl TryFrom<&str> for WindowsClientOS {
     type Error = String;
 
     fn try_from(value: &str) -> Result<Self, Self::Error> {
         if let Ok(windows) = Windows11::try_from(value) {
-            Ok(Windows::Windows11(windows))
+            Ok(WindowsClientOS::Windows11(windows))
         } else if let Ok(windows) = Windows10::try_from(value) {
-            Ok(Windows::Windows10(windows))
+            Ok(WindowsClientOS::Windows10(windows))
         } else if let Ok(windows) = Windows8::try_from(value) {
-            Ok(Windows::Windows8(windows))
+            Ok(WindowsClientOS::Windows8(windows))
         } else if let Ok(windows) = Windows7::try_from(value) {
-            Ok(Windows::Windows7(windows))
+            Ok(WindowsClientOS::Windows7(windows))
         } else if let Ok(windows) = WindowsVista::try_from(value) {
-            Ok(Windows::WindowsVista(windows))
+            Ok(WindowsClientOS::WindowsVista(windows))
         } else if let Ok(windows) = WindowsXP::try_from(value) {
-            Ok(Windows::WindowsXP(windows))
+            Ok(WindowsClientOS::WindowsXP(windows))
         } else {
             Err(format!("Not a windows: {}", value))
         }
